b3ca3681e9015c19edc88f14c7035bb537950b41
gustavo003 <d.gustavo.sd2@gmail.com>
2020-08-04 17:27:53 -0300

    Second Commit

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const conection = new Sequelize('Perguntas', 'zero3', 'onmargatesands',{
+const conection = new Sequelize('Perguntas', '(Coloque o nome de usuario do seu banco de dados aqui)', '(coloque a sua senha aqui)',{
 
 
     host: 'localhost',
